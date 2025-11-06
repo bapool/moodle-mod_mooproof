c145c23063cded4fbed56580fac88b4f615c9baf
bapool <bpool@nationaltrail.us>
2025-11-06 18:34:43 -0500

    Add files via upload

--- mooproof/db/access.php
+++ mooproof/db/access.php
@@ -4,8 +4,15 @@
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
+// any later version.
+/**
+ * Privacy Subsystem implementation for mod_mooproof
+ *
+ * @package    mod_mooproof
+ * @copyright  2025 Brian A. Pool
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
